9def3ea029716ddede686e258d61aa7317e6ae7c
lsamaria <lsamaria@aol.com>
2019-11-15 05:25:31 -0500

    Initial Commit

---
+++ SwiptToDismiss/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiptToDismiss
+//
+//  Created by Lance Samaria on 11/15/19.
+//  Copyright © 2019 Lance Samaria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
